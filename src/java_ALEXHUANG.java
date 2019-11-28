833ff617a9a49eb5b3dc4ce7b2e5559dc9df761a
rmit-s3628653-alex-huang <s3628653@student.rmit.edu.au>
2019-11-29 00:14:46 +1100

    adding parameters and input

--- src/java_ALEXHUANG.java
+++ src/java_ALEXHUANG.java
@@ -1,15 +1,33 @@
 // NAB Technology Internship Program - Coding Challenge
-// Completed by ALEX HUANG
+// Completed by ALEX HUANG - 0424224782 - ahuang4321@gmail.com
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class java_ALEXHUANG {
 
 	public static void main(String[] args) {
-		NABTest();
+		Scanner keyboard = new Scanner(System.in);
+		while (true) { // loop until user terminate
+			System.out.println(
+					"Enter your string\nor type 'N' to use default N string\n" + "or 'E' to terminate program: ");
+			String NABString = keyboard.nextLine();
+
+			if (NABString.contentEquals("N")) { // default string
+				NABTest(N);
+			} else if (NABString.equals("E")) { // terminate
+				System.out.println("Program Terminated");
+				break;
+			} else {
+				NABTest(NABString);
+			}
+			System.out.println();
+		}
+		keyboard.close();
 	}
 
 	// N is string with the following possible conditions:
-	// (a) empty, (b) numeric, (c) non-numeric, (d) mixed , (e) case sensitive
+	// (a) empty, (b) numeric, (c) non-numeric, (d) mixed, (e) case sensitive
 	// String was given 'static' keyword as java cannot make a static reference
 	// to a non-static variable
 	static String N = "abcabc";
@@ -17,33 +35,37 @@ class java_ALEXHUANG {
 	// Determine which substrings repeat as well as how many there are
 	// eg. input: "123123" output: 6 substrings: 1,12,123,2,23,3
 	// eg. input: "abcabc" output: 6 substrings: a,ab,abc,b,bc,c
-	public static void NABTest() {
+	public static void NABTest(String N) {
 
-		int counter = 0;
+		int counter = 0; // counter counting the number of repeated substrings
 		ArrayList<String> repArray = new ArrayList<String>();
-		String tempSub;
-		String tempComp;
+		String tempSub; // base substring to compare
+		String tempComp; // secondary substring to compare to tempSub
 		int compLength;
 
+		// Create one substring and then compare to other substrings
 		for (int i = 0; i < N.length(); i++) {
-			
+
 			for (int j = 1; j < N.length(); j++) {
-				
-				if (i + j <= N.length() && j <= N.length() / 2 && j <= N.length() - (i + j)) {
+
+				// make i+j doesn't go out of bounds
+				// for efficiency purposes N.length/2 to avoid looping through substrings that
+				// is impossible
+				if (i + j <= N.length() && j <= N.length() / 2) {
 					tempSub = N.substring(i, i + j);
-					compLength = tempSub.length();
-					// check if we've already checked this substring
-					
+
+					compLength = tempSub.length(); // value to make sure we only compare to same length substrings
+
 					for (int k = i + j; k < N.length(); k++) {
-						
+
 						if (k + compLength <= N.length()) {
 							tempComp = N.substring(k, k + compLength);
-							
+							// only compare if we substring hasn't been tested
 							if (repArray.contains(tempComp) == false) {
-								
+								// if two temp strings are the same that means it is repeating
 								if (tempSub.equals(tempComp)) {
-									repArray.add(tempComp);
-									counter++;
+									repArray.add(tempComp); // add to the array of repeated substrings
+									counter++; // increase repeat substring counter
 								}
 							}
 						}
