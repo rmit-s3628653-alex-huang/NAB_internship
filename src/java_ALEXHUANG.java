307537975297338ed514628fea120de4bfdabb97
rmit-s3628653-alex-huang <s3628653@student.rmit.edu.au>
2019-11-28 19:40:11 +1100

    fixes
    current iteration seems to have issues with symbols

--- src/java_ALEXHUANG.java
+++ src/java_ALEXHUANG.java
@@ -8,22 +8,25 @@ public class java_ALEXHUANG {
 
 	// N is string with the following possible conditions:
 	// (a) empty, (b) numeric, (c) non-numeric, (d) mixed , (e) case sensitive
-	static String N = "&&abab";
+	static String N = "abcabc";
 
 	// Determine which substrings repeat as well as how many there are
 	// eg. input: "123123" output: 6 substrings: 1,12,123,2,23,3
 	private static void NABTest() {
 		int counter = 0;
 		ArrayList<String> repArray = new ArrayList<String>();
+		String tempSub;
+		String tempString;
 		for (int i = 0; i < N.length(); i++) {
-			for (int j = 1; j <= N.length(); j++) {
+			for (int j = 1; j < N.length(); j++) {
 				if (i + j <= N.length()) {
-					String tempSub = N.substring(i, i + j);
-					String tempString = N;
+					tempSub = N.substring(i, i + j);
+					tempString = N;
 
-					// check if we've already check this substring
+					// check if we've already checked this substring
 					if (repArray.contains(tempSub) == false) {
 						// remove substring from the string
+						// replace with space to prevent creation of incorrect string for comparison
 						tempString = tempString.replaceFirst(tempSub, " ");
 						if (tempString.contains(tempSub)) {
 							repArray.add(tempSub);
